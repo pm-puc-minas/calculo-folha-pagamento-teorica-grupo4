bdbc36dc94e3c1a10fbf158001fcecb61d761abf
Gabriel dos Santos Silva Coelho <gabriel.coelho@sga.pucminas.br>
2025-10-04 12:51:50 -0300

    add novo front end

---
+++ FrontEnd/src/pages/auth/sign-in.jsx
@@ -0,0 +1,79 @@
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
+import { Link } from "react-router-dom";
+
+
+export function SignIn() {
+  return (
+    <section className="m-8 flex gap-4">
+      <div className="w-full lg:w-3/5 mt-24">
+        <div className="text-center">
+          <Typography variant="h2" className="font-bold mb-4">Login</Typography>
+          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Digite o seu email para realizar o Login.</Typography>
+        </div>
+        <form className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2">
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
+              Seu email
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="name@mail.com"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+            <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
+              Senha
+            </Typography>
+            <Input
+              type="password"
+              size="lg"
+              placeholder="********"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+          </div>
+          <Button className="mt-6" fullWidth>
+            Logar
+          </Button>
+
+          <div className="flex items-center justify-between gap-2 mt-6">
+            <Checkbox
+              label={
+                <Typography
+                  variant="small"
+                  color="gray"
+                  className="flex items-center justify-start font-medium"
+                >
+                  Subscribe me to newsletter
+                </Typography>
+              }
+              containerProps={{ className: "-ml-2.5" }}
+            />
+          </div>
+        </form>
+
+      </div>
+      <div className="w-2/5 h-full hidden lg:block">
+        <img
+          src="/img/pattern.png"
+          className="h-full w-full object-cover rounded-3xl"
+        />
+      </div>
+
+    </section>
+  );
+}
+
+export default SignIn;
+
+
