c295bbfa0b5543c1e9bb08de987885cd01587e0b
Gabriel dos Santos Silva Coelho <gabriel.coelho@sga.pucminas.br>
2025-10-10 12:57:10 -0300

    corrigindo a lógica da herança e refinamento final

--- BackEnd/backend/src/main/java/com/trabalho/backend/model/Administrador.java
+++ BackEnd/backend/src/main/java/com/trabalho/backend/model/Administrador.java
@@ -3,12 +3,39 @@ package com.trabalho.backend.model;
 import jakarta.persistence.Entity;
 
 @Entity
-public class Administrador {
+public class Administrador extends Usuario {
 
     private Long idAdministrador;
+    private String senha;
+    private String cpf;
 
     public Long getId() {
         return idAdministrador;
     }
+
+    public Long getIdAdministrador() {
+        return idAdministrador;
+    }
+
+    public void setIdAdministrador(Long idAdministrador) {
+        this.idAdministrador = idAdministrador;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
     
 }
